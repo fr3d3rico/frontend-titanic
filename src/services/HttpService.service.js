3b04a8fea2f8748d59ab0bc26d191c15901ba2d4
Fred <frederico.a.soares@gmail.com>
2020-12-19 17:28:40 -0300

    Alterado parte de http inicial

--- src/services/HttpService.service.js
+++ src/services/HttpService.service.js
@@ -1,34 +1,5 @@
 import Api from '../config/Api.config';
 
-const HttpService = () => {
-
-    return {
-        httpAsync: httpAsync,
-        httpSync: httpSync,
-    };
-
-}
-
-const httpAsync = (endpoint) => {
+export const HttpService = (endpoint) =>
     fetch(Api.url + endpoint)
-        .then(response => response.json())
-        .then(result => {
-            return result;
-        })
-        .catch(e => {
-            console.log(e);
-        });
-}
-
-const httpSync = async (endpoint) => {
-    await fetch(Api.url + endpoint)
-        .then(response => response.json())
-        .then(result => {
-            return result;
-        })
-        .catch(e => {
-            console.log(e);
-        });
-}
-
-export default HttpService;
+        .then(response => response.json());
