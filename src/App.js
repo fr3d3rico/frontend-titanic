fac8179bc43c9d5b819352c94dd5322f56a1d63e
Fred <frederico.a.soares@gmail.com>
2020-12-26 13:11:08 -0300

    Para melhorar um pouco, retirei tudo do App.js e coloquei no Home. Acho que terá que alterar ainda, mas pelo menos a tela já tem um cara mais organizada.

--- src/App.js
+++ src/App.js
@@ -1,59 +1,9 @@
 import React from 'react';
-import { MemoryRouter, Switch, Route } from 'react-router-dom';
 
-import Login from './components/Login.component';
 import Home from './components/Home.component';
-import About from './components/About.component';
-import Users from './components/Users.component';
-
-import Jumbotron from 'react-bootstrap/Jumbotron';
-import Container from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/Button';
-import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
-import { LinkContainer } from 'react-router-bootstrap';
 
 const App = () => (
-  <MemoryRouter>
-    <Container className="p-3">
-      <Jumbotron>
-        <h1 className="header">Welcome To React-Bootstrap</h1>
-        <h2>
-          Current Page is{' '}
-          <Switch>
-            <Route path="/about">
-              <About />
-            </Route>
-            <Route path="/users">
-              <Users />
-            </Route>
-            <Route path="/login">
-              <Login />
-            </Route>
-            <Route path="/">
-              <Home />
-            </Route>
-          </Switch>
-        </h2>
-        <h2>
-          Navigate to{' '}
-          <ButtonToolbar className="custom-btn-toolbar">
-            <LinkContainer to="/">
-              <Button>Home</Button>
-            </LinkContainer>
-            <LinkContainer to="/about">
-              <Button>About</Button>
-            </LinkContainer>
-            <LinkContainer to="/users">
-              <Button>Users</Button>
-            </LinkContainer>
-            <LinkContainer to="/login">
-              <Button>Login</Button>
-            </LinkContainer>
-          </ButtonToolbar>
-        </h2>
-      </Jumbotron>
-    </Container>
-  </MemoryRouter>
+  <Home />
 );
 
 export default App;
