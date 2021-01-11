4d13ae4b75233e8d429a072f384ab25a35a7b6a2
Fred <frederico.a.soares@gmail.com>
2021-01-11 11:23:46 -0300

    Melhoria na validação do login.

--- src/components/login/Login.validation.js
+++ src/components/login/Login.validation.js
@@ -1,10 +1,14 @@
 import Joi from 'joi';
 
 const loginValidation = (data) => {
+
+    const usuario_tamanho_minimo = 6;
+    const senha_tamanho_minimo = 6;
+
     const Schema = Joi.object({
-        usuario: Joi.string().min(6).required().error(new Error('Usuário deve ter no mínimo 6 caracteres')),
-        senha: Joi.string().min(6).required().error(new Error('Senha deve ter no mínimo 6 caracteres'))
-    })
+        usuario: Joi.string().min(usuario_tamanho_minimo).required().error(new Error(`Usuário deve ter no mínimo ${usuario_tamanho_minimo} caracteres`)),
+        senha: Joi.string().min(senha_tamanho_minimo).required().error(new Error(`Senha deve ter no mínimo ${senha_tamanho_minimo} caracteres`))
+    });
 
     return Schema.validate(data);
 }
